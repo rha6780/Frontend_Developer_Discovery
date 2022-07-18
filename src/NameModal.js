b8251ee3b5a30b0e83980ca66d3afd1411502157
rha6780 <rha4578@gmail.com>
2022-07-18 22:37:01 +0900

    모달 컴포넌트 추가

---
+++ src/NameModal.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import './NameModal.scss';
+
+const body = document.querySelector('#body');
+class NameModal extends Component {
+    render() {
+        const { nameModal } = this.props;
+
+        componentDidMount() {
+            body.setAttribute("style", "overflow: hidden;");
+            this.setState({status: true});
+        }
+
+        componentWillUnmount() {
+            body.setAttribute("style");
+            this.setState({status: false});
+        }
+
+        return(
+            <div class="NameModal">
+                <div> 모달 테스트</div>
+            </div>
+        );
+    }
+}
+
+export default NameModal;
