d983c3f994c391c4aedd22412fdbf57dcf8824cc
rha6780 <rha4578@gmail.com>
2022-08-15 21:59:51 +0900

    fix: 불필요한 부분 제거

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -12,11 +12,6 @@ class Main extends Component {
                     <h1 className="project-title"> <span className="upper-case">D</span>eveloper <span className="upper-case">D</span>iscovery </h1>
 
                     <div className="sub-area">
-                        <div className="sub-title">프로젝트 취지</div>
-                        <div> - 미래에 어떤 일을 할지, 직군 등은 직접 그 일을 하기 전까지 결정하기 어렵다는 문제점이 있습니다.</div>
-                        <div> - 간단한 QnA를 통해서 간접적으로 직군별, 어떤 일을 하는지 느끼며 재미를 주기 위해 구성하였습니다.</div>
-                    </div>
-                    <div className="sub-area">
                         <div className="sub-title">주의</div>
                         <div>환경에 따라서 동일 직군이라도 경험하는 바가 다르기 때문에 해당 결과에 대한 성급한 일반화는 지양해주시기 바랍니다. (재미로 봐주세요)</div>
 
