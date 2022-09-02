e501b0feb2bc6be1d994834013c5be01e759d23c
rha6780 <rha4578@gmail.com>
2022-09-02 22:09:03 +0900

    feat: component 코드 tsx파일로 전환

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as NavBar } from './shared/NavBar.js';
-export { default as Main } from './Home/Main.js';
+export { default as Main } from './Home/Main.tsx';
 export { default as Graph } from './Home/Graph.tsx';
 export { default as Question } from './QnA/Question.tsx';
 export { default as Answer } from './QnA/Answer.tsx';
