9a78c000f835516a9aeb16b695b33af68fb6d514
rha6780 <rha4578@gmail.com>
2022-10-30 17:17:06 +0900

    fix: 빈 api 시 홈 화면으로 가도록 수정 및 TODO 작성

--- src/components/QnA/Question.tsx
+++ src/components/QnA/Question.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import '../../asset/css/QnA.css';
 import { QuestionState } from '../../models/Question';
 import { getQuestions } from '../../api/index';
@@ -6,12 +7,17 @@ import { getQuestions } from '../../api/index';
 const Question = (props) => {
     const [questions, setQuestion] = useState<QuestionState[]>();
     const question_id = props.id;
+    const history = useHistory();
 
 
     useEffect(() => {
         const initQuestion = async () => {
             const questions = await getQuestions(question_id);
             setQuestion(questions);
+            if (questions.length === 0) {
+                // TODO: 결과 페이지로 가도록
+                history.push("/");
+            }
         };
 
         initQuestion();
