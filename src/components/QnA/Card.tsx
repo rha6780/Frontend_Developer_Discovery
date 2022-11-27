79979a7af6251b9e224cc82b629becb3ce8c1c1b
rha6780 <rha4578@gmail.com>
2022-11-27 13:27:24 +0900

    feat: card 컴포넌트 추라

--- src/components/QnA/Card.tsx
+++ src/components/QnA/Card.tsx
@@ -6,7 +6,7 @@ import { getQuestionsList } from '../../api/index';
 
 const Card = (props) => {
     const category = props.category;
-    const question_id = useState(1);
+    let question_id = 1;
     const [questions_list, setQuestionList] = useState<QuestionState[]>();
 
     useEffect(() => {
