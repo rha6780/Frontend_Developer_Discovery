345195dc9aee6645ce2314cfa085454b902e515e
rha6780 <rha4578@gmail.com>
2023-05-14 17:53:32 +0900

    feat: hero image 추가

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -3,8 +3,12 @@ import styles from '../../../styles/Hero.module.css'
 export const Hero = () => {
     return (
         <div className={styles.container}>
-            <img src='banner.jpg' height='600px' width='100%' />
-            Developer Discovery
+            <img src='keyboard.jpg' height='600px' width='100%' />
+            <div className={styles.image_text}>
+                Developer Discovery <br />
+                개발과 관련된 정보들을 만나보세요.
+
+            </div>
         </div >
     );
 }
