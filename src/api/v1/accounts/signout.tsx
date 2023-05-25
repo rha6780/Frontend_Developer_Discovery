89c07dfb9f5038ff88672cc6eedec8978c56a585
rha6780 <rha4578@gmail.com>
2023-05-25 23:29:22 +0900

    fix: 로그 아웃 시 토큰 제거

--- src/api/v1/accounts/signout.tsx
+++ src/api/v1/accounts/signout.tsx
@@ -4,6 +4,7 @@ import { removeCookie } from '@/api/cookies';
 export const signOut = async () => {
     try {
         removeCookie('refresh_token');
+        removeCookie('access_token');
     } catch (error) {
         console.log(error);
     }
