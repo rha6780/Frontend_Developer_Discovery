b9a0fe4c86f8546623048d3051676e38dc7394dc
rha6780 <rha4578@gmail.com>
2023-05-26 00:10:59 +0900

    feat: editor component add

---
+++ src/components/Post/Head.tsx
@@ -0,0 +1,13 @@
+import styles from '../../../styles/Post.module.css'
+
+export const Head = () => {
+
+    return (
+        <div className={styles.editor_title}>
+            글 작성하기
+        </div >
+
+    );
+}
+
+export default Head;
--- src/pages/post/write.tsx
+++ src/pages/post/write.tsx
@@ -1,14 +1,22 @@
+import styles from '../../../styles/Post.module.css'
 import NavBar from '../../components/shared/NavBar';
 import Footer from '../../components/shared/Footer';
-import { EmailCheck } from '../../components/Account/Emailcheck';
+import Head from '../../components/Post/Head';
+import dynamic from 'next/dynamic'
+import 'react-quill/dist/quill.snow.css';
 
+const EditorWrapper = dynamic(() => import('react-quill'), {
+    ssr: false,
+    loading: () => <p> ⏱ Loading </p>,
+})
 
 const Page = () => {
     return (
         <div>
             <NavBar />
-            <div className="main">
-                <EmailCheck />
+            <Head />
+            <div className={styles.main_body}>
+                <EditorWrapper style={{ height: '400px' }} />
             </div>
             <Footer />
         </div>
