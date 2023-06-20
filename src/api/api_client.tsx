54e3efe1f01b976e1816a4ead0385ab09892c941
rha6780 <rha4578@gmail.com>
2023-06-20 17:55:05 +0900

    feat: TODO 추가

--- src/api/api_client.tsx
+++ src/api/api_client.tsx
@@ -3,6 +3,7 @@ import { getAccessToken } from './cookies';
 
 // axios.defaults.withCredentials = true;
 
+// TODO: stage 별로 url 등 변경할 수 있도록
 export const ApiClient = axios.create({
     baseURL: "http://ec2-43-200-202-244.ap-northeast-2.compute.amazonaws.com",
     headers: {
