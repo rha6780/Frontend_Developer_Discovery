72ef2f0bb1bb8ed5f6abaf770ed101130180ad4a
rha6780 <rha4578@gmail.com>
2023-07-08 20:59:31 +0900

    fix: seo 관련 내용 제거

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,21 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>developerdiscovery</title>
-        <meta
-          name="description"
-          content={
-            "개발자들의 유용한 정보를 공유하자!"
-          }
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="developerdiscovery" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://developerdiscovery.com" />
-        <meta property="og:image" content="" />
-        <meta property="og:article:author" content="rha6780" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
