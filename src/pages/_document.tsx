e75ca23a51422ea9a25eeb27b3474cad2e1d345f
rha6780 <rha4578@gmail.com>
2023-07-08 17:58:00 +0900

    fix: seo icon 이미지 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,8 +15,9 @@ export default function Document() {
         <meta property="og:title" content="developerdiscovery" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://developerdiscovery.com" />
-        <meta property="og:image" content="" />
+        <meta property="og:image" content="/assets/favicon.ico" />
         <meta property="og:article:author" content="rha6780" />
+        <link rel="icon" href="/assets/favicon.ico"></link>
       </Head>
       <body>
         <Main />
