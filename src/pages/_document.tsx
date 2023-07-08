a973a5be36d341b4d7698cfcc7e6d8fdd63d766c
rha6780 <rha4578@gmail.com>
2023-07-08 17:42:39 +0900

    feat: SEO 관련 값 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Seo from '@/components/shared/SEO'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>developerdiscovery</title>
+        <meta
+          name="description"
+          content={
+            "개발자들의 유용한 정보를 공유하자!"
+          }
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="developerdiscovery" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://developerdiscovery.com" />
+        <meta property="og:image" content="" />
+        <meta property="og:article:author" content="rha6780" />
+      </Head>
       <body>
         <Main />
         <NextScript />
